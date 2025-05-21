0372f3b799aad804271b325fe6edc6215b9df13e
David Felipe Ruiz Figueroa <143728626+DavidRuizFig@users.noreply.github.com>
2025-05-20 22:32:15 -0500

    Create perro.py

---
+++ Punto 3/TiendaMascotas/perro.py
@@ -0,0 +1,11 @@
+from .mascota import Mascota
+
+class Perro(Mascota):
+    def __init__(self, nombre, edad, color, peso, muerde):
+        super().__init__(nombre, edad, color)
+        self.peso = peso
+        self.muerde = muerde
+
+    @staticmethod
+    def sonido():
+        print("Los perros ladran")
