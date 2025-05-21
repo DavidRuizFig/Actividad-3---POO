a619141b1213523c1958e9cb6e00c465eada9afd
David Felipe Ruiz Figueroa <143728626+DavidRuizFig@users.noreply.github.com>
2025-05-20 22:48:28 -0500

    Create profesor.py

---
+++ Punto 5/profesores/profesor.py
@@ -0,0 +1,12 @@
+class Profesor:
+    """
+    Esta clase denominada Profesor es una superclase que representa un
+    profesor genérico.
+    """
+
+    def imprimir(self):
+        """
+        Método que imprime en pantalla un texto específico identificando
+        que el objeto es un Profesor.
+        """
+        print("Es un profesor.")
