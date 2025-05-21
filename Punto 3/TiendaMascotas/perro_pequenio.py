dcec7c16846d07614856772bf604e99cf7539852
David Felipe Ruiz Figueroa <143728626+DavidRuizFig@users.noreply.github.com>
2025-05-20 22:38:00 -0500

    Create perro_pequenio.py

---
+++ Punto 3/TiendaMascotas/perro_pequenio.py
@@ -0,0 +1,16 @@
+from .perro import Perro
+
+class PerroPequenio(Perro):
+    pass
+
+class Caniche(PerroPequenio):
+    pass
+
+class YorkshireTerrier(PerroPequenio):
+    pass
+
+class Schnauzer(PerroPequenio):
+    pass
+
+class Chihuahua(PerroPequenio):
+    pass
